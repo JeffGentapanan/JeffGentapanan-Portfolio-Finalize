eede5d8256628e41482d0bf7355bea4bf40c3ba7
Jeff Gentapanan <jeff.gentapanan2004525@gmail.com>
2026-04-06 01:16:06 +0800

    Add Plasma Background kag iba pa nga changes yata basta muna takan nako wmahhaha syempre pabulig ai para magana

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,32 @@ import Skills from './components/Skills';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Plasma from './Plasma';
 import './index.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ position: 'relative', backgroundColor: '#000' }}>
+      
+      {/* ?Background Plasma Layer */}
+      <div style={{ 
+        width: '100vw', 
+        height: '100vh', 
+        position: 'fixed', 
+        top: 0, 
+        left: 0, 
+        zIndex: -1,
+        backgroundColor: '#000'
+       }}>
+       <Plasma 
+        color="#ffffff"
+        speed={0.6}
+        direction="forward"
+        scale={1.1}
+        opacity={0.8}
+        mouseInteractive={false}
+       />
+      </div>
       <Header />
       <main className="main-content">
         <About />
